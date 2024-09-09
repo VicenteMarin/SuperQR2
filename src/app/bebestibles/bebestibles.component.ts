15541da58b2b1ced2578ce6b5d2dd20e38594751
Baltiuss <bescalanter@correo.uss.cl>
2024-09-08 23:01:35 -0300

    Bebestibles

--- src/app/bebestibles/bebestibles.component.ts
+++ src/app/bebestibles/bebestibles.component.ts
@@ -1,12 +1,73 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-bebestibles',
   standalone: true,
   imports: [],
   templateUrl: './bebestibles.component.html',
-  styleUrl: './bebestibles.component.css'
+  styleUrls: ['./bebestibles.component.css'] // Cambiado a 'styleUrls'
 })
 export class BebestiblesComponent {
+  
+  bebestiblesPrincipales: any[];
 
+  constructor() {
+    this.bebestiblesPrincipales = [];
+    this.bebestiblesPrueba();
+    this.getBebestibles(); // Llama a getBebestibles para obtener datos reales
+  }
+
+  async bebestiblesPrueba() {
+    let bebestiblesPrueba = [
+      {
+        nombre_bebestible: 'Mojito Clásico',
+        descripcion: 'Refrescante mezcla de ron, menta fresca, jugo de limón y soda, perfecto para un día caluroso.',
+        precio: 6500
+      },
+      {
+        nombre_bebestible: 'Cerveza Artesanal',
+        descripcion: 'Cerveza de elaboración artesanal, con un sabor amargo característico y aromas cítricos.',
+        precio: 5000
+      },
+      {
+        nombre_bebestible: 'Jugo Natural de Frutas',
+        descripcion: 'Delicioso jugo recién exprimido, disponible en sabores como naranja, frutilla o piña.',
+        precio: 3200
+      }
+    ];
+    this.bebestiblesPrincipales = bebestiblesPrueba;
+  }
+
+  async getBebestibles() {
+    try {
+      const result = await fetch(''); // URL válida
+      if (!result.ok) {
+        throw new Error('Error en la petición');
+      }
+      const response = (await result.json()) as any[];
+      this.bebestiblesPrincipales = response;
+    } catch (error) {
+      console.error('Error al obtener los bebestibles:', error);
+    }
+  }
+  mostrarInfo(bebestible: any){
+    Swal.fire({
+      title: `${bebestible.nombre_bebestible}`,  
+      html: `${bebestible.descripcion}<br>Valor: $${bebestible.precio}`,  // Sigue siendo descripción y precio
+      confirmButtonText: 'Agregar al carrito',
+      confirmButtonColor: '#71cf13',
+      cancelButtonText: 'Cancelar',
+      cancelButtonColor: '#DBDBDB',
+      showCloseButton: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire("¡Producto agregado!", "", "success");
+      }
+    });
+  }
 }
