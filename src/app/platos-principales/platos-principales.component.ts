2d2496e177ea3514f60eb8c9eaaeaad621e58f6f
VicenteMarin <vmarina3@correo.uss.cl>
2024-10-03 23:31:24 -0300

    1.1
    Base de datos funcional

--- src/app/platos-principales/platos-principales.component.ts
+++ src/app/platos-principales/platos-principales.component.ts
@@ -4,6 +4,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import Swal from 'sweetalert2';
+import { SupabaseService } from '../supabase.service';
 
 @Component({
   selector: 'app-platos-principales',
@@ -16,27 +17,28 @@ export class PlatosPrincipalesComponent {
 
 
   platosPrincipales: any[];
-  constructor(){
+  constructor(private sus: SupabaseService){
     this.platosPrincipales = [];
-    this.platosPrueba();
-    //this.getPlatos();
+    //this.platosPrueba();
+    console.log("paso 0");
+    this.getPlatos();
   }
 
   
   async platosPrueba(){
     let platosPrueba = [
       {
-        nombre_plato: 'Fetuccini bolognesa',
+        nombre_plato_principal: 'Fetuccini bolognesa',
         descripcion: 'Deliciosos fetuccinies de pasta casera, con salsa bolognesa de una receta secreta del restaurant.',
         precio: '13900'
       },
       {
-        nombre_plato: 'Pollo a las brasas con papas fritas',
+        nombre_plato_principal: 'Pollo a las brasas con papas fritas',
         descripcion: 'Pollo asado a las brasas, junto a una salsa especial acompañado de nuestras clásicas papas rústicas con salsa del himalaya.',
         precio: '11900'
       },
       {
-        nombre_plato: 'Carne mechada con puré',
+        nombre_plato_principal: 'Carne mechada con puré',
         descripcion: 'Nuestra colación clásica, pero con pequeños cambios de nuestro chef para darle un toque premium.',
         precio: '12500'
       }
@@ -45,14 +47,18 @@ export class PlatosPrincipalesComponent {
   }
 
   async getPlatos(){
-    const result = await fetch('');
-    const response = (await result.json()) as any[];
-    this.platosPrincipales = response;
+    //const result = await fetch('');
+    //const response = (await result.json()) as any[];
+    //this.platosPrincipales = response;
+    console.log("Paso 1");
+    this.platosPrincipales = await this.sus.getPlatos();
+    console.log("Paso 3");
+    console.log(this.platosPrincipales);
   }
 
   mostrarInfo(plato: any){
     Swal.fire({
-      title: `${plato.nombre_plato}`,
+      title: `${plato.nombre_plato_principal}`,
       html: `${plato.descripcion}<br>Valor: $${plato.precio}`,
       confirmButtonText: 'Agregar al carrito',
       confirmButtonColor: '#71cf13',
---
+++ src/app/supabase.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SupabaseService {
+  private supabase: SupabaseClient;
+
+  constructor() {
+    this.supabase = createClient('https://utzigyzeijlgmmgojnrw.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0emlneXplaWpsZ21tZ29qbnJ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjYyODIwOTQsImV4cCI6MjA0MTg1ODA5NH0.PZJb53h5yJ5VFzG7FJar1qCDZBYYD5jH2odMeFngsJo');
+  }
+
+  async insertOrder(orderData: any) {
+    const { data, error } = await this.supabase
+      .from('pedido')
+      .insert(orderData);
+
+    if (error) {
+      throw new Error('Error inserting order: ' + error.message);
+    }
+
+    return data;
+  }
+
+  async getPlatos() {
+    const datos = await this.supabase
+    .from('plato_principal')
+    .select('*')
+    console.log("Paso 2");
+    return datos.data || []
+  }
+
+  async insertOrderDetail(orderDetailData: any) {
+    const { data, error } = await this.supabase
+      .from('pedido_detalle')
+      .insert(orderDetailData);
+
+    if (error) {
+      throw new Error('Error inserting order detail: ' + error.message);
+    }
+
+    return data;
+  }
+}
